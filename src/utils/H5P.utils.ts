1029036987cfc4a5d660a649b3ac215d46895234
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-05-25 15:24:25 +0200

    add functions for registering content types and widgets

--- src/utils/H5P.utils.ts
+++ src/utils/H5P.utils.ts
@@ -1,4 +1,9 @@
-import type { H5PEditorObject, H5PObject } from "h5p-types";
+import type {
+  H5PEditorObject,
+  H5PObject,
+  IH5PContentType,
+  IH5PWidget,
+} from "h5p-types";
 
 export const H5P: H5PObject = (window as any).H5P ?? {};
 export const H5PEditor: H5PEditorObject = (window as any).H5PEditor ?? {};
@@ -26,4 +31,25 @@ export const getImageUrl = (imagePath: string | undefined): string | null => {
     : getAbsoluteUrlFromRelativePath(imagePath);
 
   return imageUrl;
+};
+
+export const registerContentType = (
+  name: string,
+  contentType: IH5PContentType,
+): void => {
+  (H5P as any)[name] = contentType;
+};
+
+/**
+ * @param h5pName The widget's logical name, usually in PascalCase
+ * @param widgetName The name that's used when using the widget in semantics.json (e. g. `html`, `showWhen`)
+ * @param widget
+ */
+export const registerWidget = (
+  h5pName: string,
+  widgetName: string,
+  widget: IH5PWidget,
+): void => {
+  H5PEditor[h5pName] = widget;
+  H5PEditor.widgets[widgetName] = widget;
 };
