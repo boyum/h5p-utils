c366f3659582d50abc8696179e4ada509da85091
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-08-24 20:51:39 +0200

    fix typo

--- src/models/H5PContentType.ts
+++ src/models/H5PContentType.ts
@@ -5,7 +5,7 @@ export abstract class H5PContentType<
     TParams extends Record<string, unknown> | unknown = unknown,
   >
   extends H5P.EventDispatcher
-  implements IH5PContentType<TParams>
+  implements IH5PContentType
 {
   protected wrapper: HTMLElement;
 
