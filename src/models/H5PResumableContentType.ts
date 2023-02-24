9c49b1d1a1208e05725998f4c8029205ab6f28ac
Sindre Bøyum <sindre.boyum@tietoevry.com>
2023-02-24 12:10:12 +0100

    fix(resumable content type): get state from `extras`

--- src/models/H5PResumableContentType.ts
+++ src/models/H5PResumableContentType.ts
@@ -1,6 +1,8 @@
 import type { H5PExtras, IH5PResumableType } from "h5p-types";
 import { H5PContentType } from "./H5PContentType.js";
 
+type ExtrasWithState<TState> = H5PExtras & { previousState?: TState };
+
 export abstract class H5PResumableContentType<
     TParams = unknown,
     TState = unknown,
@@ -8,16 +10,17 @@ export abstract class H5PResumableContentType<
   extends H5PContentType<TParams>
   implements IH5PResumableType<TState>
 {
+  protected override extras: ExtrasWithState<TState> | undefined;
   protected state: TState | undefined;
 
   constructor(
     params: TParams,
     contentId: string,
-    extras?: H5PExtras,
-    previousState?: TState,
+    extras?: ExtrasWithState<TState>,
   ) {
     super(params, contentId, extras);
-    this.state = previousState;
+    this.extras = extras;
+    this.state = extras?.previousState;
   }
 
   abstract getCurrentState(): TState | undefined;
